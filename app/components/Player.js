1531ad83b8aaacb0c9b862950ee04d6ec76da839
crbentle <leroybentley@yahoo.com>
2017-04-18 17:01:52 -0400

    Added song playing capability
    Using HTML5 AudioContext to play songs.

--- app/components/Player.js
+++ app/components/Player.js
@@ -1,8 +1,46 @@
 var React = require('react');
 var PropTypes = require('prop-types');
+var songApi = require('../utils/songApi');
+const PlayList = require('./PlayList');
 var Glyphicon = require('react-bootstrap').Glyphicon;
+var axios = require('axios');
+
+class Controller extends React.Component {
+  render() {
+    return (
+      <div className='player'>
+        <div className='player-content'>
+          <Control handleClick={this.props.handleClick.bind(null, 'previous')} icon='fast-backward'/>
+          <Play isPlaying={this.props.isPlaying} handleClick={this.props.handleClick} icon='play'/>
+          <Control handleClick={this.props.handleClick.bind(null, 'next')} icon='fast-forward'/>
+          <VolumeControl volume='40'/>
+        </div>
+      </div>
+    )
+  }
+}
+
+class ProgressBar extends React.Component {
+  render() {
+      var style = {
+        width: this.props.progress + '%'
+      };
+    return(
+      <div onClick={this.props.handleClick} className='player-progress'>
+        <div style={style} className='progress-played'>
+          <span className='progress-slider-disc'>
+          </span>
+        </div>
+        <div className='progress-remaining'></div>
+      </div>
+    )
+  }
+}
 
 class Control extends React.Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
     return (
       <div onClick={this.props.handleClick} className='control'>
@@ -12,24 +50,10 @@ class Control extends React.Component {
   }
 }
 class Play extends Control {
-  constructor(props) {
-    super(props);
-    this.state = {
-      playing: true
-    }
-
-    this.handleClick = this.handleClick.bind(this);
-  }
-  handleClick(event) {
-    var playing = !this.state.playing;
-    this.setState( {
-      playing: playing
-    })
-  }
   render() {
-    var iconName = this.state.playing ? 'play' : 'pause';
+    var iconName = this.props.isPlaying ? 'pause' : 'play';
     return (
-      <Control handleClick={this.handleClick} icon={iconName} />
+      <Control handleClick={this.props.handleClick.bind(null, iconName)} icon={iconName} />
     )
   }
 }
@@ -69,42 +93,152 @@ class Player extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      progress: 0
+      progress: 0,
+      songList: songApi.getSongList(),
+      currentSongIndex: -1,
+      isPlaying: false
     }
 
-    this.handledClick = this.handledClick.bind(this);
+    window.audioContext = new(window.AudioContext || window.webKitAudioContext)(); // Our audio context
+    window.audioSource = null;
+
+    this.handleControlClick = this.handleControlClick.bind(this);
+    this.handleSongClick = this.handleSongClick.bind(this);
+    this.handleProgressClick = this.handleProgressClick.bind(this);
+    this.playSong = this.playSong.bind(this);
   }
-  handledClick(event) {
-    var percent = Math.round((event.clientX / window.outerWidth) * 100);
+  handleControlClick(action) {
+    switch (action) {
+      case 'play': // fall through
+      case 'pause':
+        this.togglePlayPause();
+        break;
+      case 'previous':
+        this.playPreviousSong();
+        break;
+      case 'next':
+      this.playNextSong();
+        break;
+      default:
+        //?
+    }
+  }
+  playCurrentSong() {
+    // If the 'play' button is clicked as the first action we need to set the current song index to '0'
+    var currentSongIndex = this.state.currentSongIndex;
+    if( currentSongIndex < 0 ) {
+      currentSongIndex = 0;
+    }
+    this.playSong(currentSongIndex);
+  }
+  playPreviousSong() {
+    var currentSongIndex = this.state.currentSongIndex;
+    currentSongIndex--;
+    if( currentSongIndex < 0 ) {
+      currentSongIndex = this.state.songList.length - 1;
+    }
+    this.handleSongClick(currentSongIndex);
+  }
+  playNextSong() {
+    var currentSongIndex = this.state.currentSongIndex;
+    currentSongIndex++;
+    if( currentSongIndex > (this.state.songList.length - 1) ) {
+      currentSongIndex = 0;
+    }
+    this.handleSongClick(currentSongIndex);
+  }
+  handleSongClick(index) {
+    if (index === this.state.currentSongIndex) {
+      this.togglePlayPause();
+      return;
+    } else {
+      this.playSong(index);
+    }
+  }
+  togglePlayPause() {
+    var play = this.state.isPlaying
+      ? false
+      : true;
+    if (play) {
+      if (window.audioContext != null && window.audioContext.state != 'running') {
+        window.audioContext.resume();
+      } else {
+        this.playCurrentSong();
+      }
+    } else {
+      if (window.audioContext != null) {
+        window.audioContext.suspend();
+      }
+    }
+
+    this.setState({isPlaying: play});
+  }
+  handleProgressClick(event) {
+    // var percent = Math.round((event.clientX / window.outerWidth) * 100);
+    var percent = (event.clientX / window.outerWidth) * 100;
     this.setState({progress: percent})
   }
+
+  playSong(index) {
+    var song = this.state.songList[index];
+    this.setState({
+      currentSongIndex: index,
+      isPlaying: true
+    });
+    var audioContext = window.audioContext;
+
+    // This is the BufferSource containing the buffered audio
+    var source = window.audioSource;
+
+    // Clear any existing audio source that we might be using
+    if (source != null) {
+      source.disconnect(audioContext.destination);
+      // Leave existing source to garbage collection
+      source = null;
+      // Update the window.audioSource in case the file load fails
+      window.audioSource = source;
+    }
+
+    //set the audio file's URL
+    var audioURL = song.path; //'../../songs/High.mp3';
+
+    axios({method: 'get', url: audioURL, responseType: 'arraybuffer'})
+    .then(function(response) {
+      //take the audio from http request and decode it in an audio buffer
+
+      var audioBuffer = null;
+
+      audioContext.decodeAudioData(response.data, function(audioBuffer) {
+
+        // In order to play the decoded samples contained in the audio buffer we need to wrap them in
+        // an AudioBufferSourceNode object. This object will stream the audio samples to any other
+        // AudioNode or AudioDestinationNode object.
+        source = audioContext.createBufferSource();
+        source.buffer = audioBuffer; // set the buffer to play to our audio buffer
+        source.connect(audioContext.destination); // connect the source to the output destinarion
+        source.start(0); // tell the audio buffer to play from the beginning
+
+        // Check for 'running' hear in case the context was previously suspended
+        if( window.audioContext.state !='running' ) {
+          window.audioContext.resume();
+        }
+        window.audioSource = source;
+      });
+
+    }).catch(function(error) {
+      console.log("Error: " + error);
+
+      this.setState({
+        isPlaying: false
+      });
+    }.bind(this));
+  }
   render() {
-    var style = {
-      width: this.state.progress + '%'
-    };
     return (
       <div>
-        <div className='player'>
-          <div className='player-content'>
-            <Control icon='fast-backward'/>
-            <Play icon='play'/>
-            <Control icon='fast-forward'/>
-            <VolumeControl volume='40'/>
-          </div>
-        </div>
-        {/* $("#test").mousemove(function(e){
-            var perc = e.offsetX/ $(this).width() * 100;
-            $(this).html(e.offsetX + " | " + perc + " perc");
-          }); */}
-        <div onClick={this.handledClick} className='player-progress'>
-          <div style={style} className='progress-played'>
-            {/* <span className='progress-slider'>I</span> */}
-            <span className='progress-slider-disc'>
-              {/* <ul><li></li></ul> */}
-            </span>
-          </div>
-          <div className='progress-remaining'></div>
-        </div>
+        <Controller isPlaying={this.state.isPlaying} handleClick={this.handleControlClick} />
+        <ProgressBar progress={this.state.progress} handleClick={this.handleProgressClick} />
+        <PlayList handleClick={this.handleSongClick} songs={this.state.songList} activeIndex={this.state.currentSongIndex} />
       </div>
     )
   }
--- app/utils/songApi.js
+++ app/utils/songApi.js
@@ -2,169 +2,154 @@ module.exports = {
   getSongList: function() {
     var songs = [
       {
-        "title": "A Living Prayer.mp3",
-        "path": "songs/A Living Prayer.mp3",
+        "title": "A Living Prayer",
+        "path": "../../../../songs/A Living Prayer.mp3",
         "duration": "3:18"
       }, {
-        "title": "Across The Universe.mp3",
-        "path": "songs/Across The Universe.mp3",
+        "title": "Across The Universe",
+        "path": "../../songs/Across The Universe.mp3",
         "duration": "3:31"
       }, {
-        "title": "All For Love.mp3",
-        "path": "songs/All For Love.mp3",
+        "title": "All For Love",
+        "path": "../../songs/All For Love.mp3",
         "duration": "3:22"
       }, {
-        "title": "Because the Night.mp3",
-        "path": "songs/Because the Night.mp3",
+        "title": "Because the Night",
+        "path": "../../songs/Because the Night.mp3",
         "duration": "2:30"
       }, {
-        "title": "Careless Whisper.mp3",
-        "path": "songs/Careless Whisper.mp3",
+        "title": "Careless Whisper",
+        "path": "../../songs/Careless Whisper.mp3",
         "duration": "3:42"
       }, {
-        "title": "Champagne Supernova.mp3",
-        "path": "songs/Champagne Supernova.mp3",
+        "title": "Champagne Supernova",
+        "path": "../../songs/Champagne Supernova.mp3",
         "duration": "5:08"
       }, {
-        "title": "Dock Of The Bay.mp3",
-        "path": "songs/Dock Of The Bay.mp3",
+        "title": "Dock Of The Bay",
+        "path": "../../songs/Dock Of The Bay.mp3",
         "duration": "1:43"
       }, {
-        "title": "Dreams.mp3",
-        "path": "songs/Dreams.mp3",
+        "title": "Dreams",
+        "path": "../../songs/Dreams.mp3",
         "duration": "3:15"
       }, {
-        "title": "Fade Into You.mp3",
-        "path": "songs/Fade Into You.mp3",
+        "title": "Fade Into You",
+        "path": "../../songs/Fade Into You.mp3",
         "duration": "4:16"
       }, {
-        "title": "Follow You Into The Dark.mp3",
-        "path": "songs/Follow You Into The Dark.mp3",
+        "title": "Follow You Into The Dark",
+        "path": "../../songs/Follow You Into The Dark.mp3",
         "duration": "3:17"
       }, {
-        "title": "Freshman.mp3",
-        "path": "songs/Freshman.mp3",
+        "title": "Freshman",
+        "path": "../../songs/Freshman.mp3",
         "duration": "3:41"
       }, {
-        "title": "Goodbye To You.mp3",
-        "path": "songs/Goodbye To You.mp3",
+        "title": "Goodbye To You",
+        "path": "../../songs/Goodbye To You.mp3",
         "duration": "3:37"
       }, {
-        "title": "Got You Where I Want You.mp3",
-        "path": "songs/Got You Where I Want You.mp3",
+        "title": "Got You Where I Want You",
+        "path": "../../songs/Got You Where I Want You.mp3",
         "duration": "3:28"
       }, {
-        "title": "High and Dry.mp3",
-        "path": "songs/High and Dry.mp3",
+        "title": "High and Dry",
+        "path": "../../songs/High and Dry.mp3",
         "duration": "4:41"
       }, {
-        "title": "High.mp3",
-        "path": "songs/High.mp3",
+        "title": "High",
+        "path": "../../songs/High.mp3",
         "duration": "3:42"
       }, {
-        "title": "I'll Be.mp3",
-        "path": "songs/I'll Be.mp3",
+        "title": "I'll Be",
+        "path": "../../songs/I'll Be.mp3",
         "duration": "4:05"
       }, {
-        "title": "Ironic.mp3",
-        "path": "songs/Ironic.mp3",
+        "title": "Ironic",
+        "path": "../../songs/Ironic.mp3",
         "duration": "3:35"
       }, {
-        "title": "Jar Of Hearts.mp3",
-        "path": "songs/Jar Of Hearts.mp3",
+        "title": "Jar Of Hearts",
+        "path": "../../songs/Jar Of Hearts.mp3",
         "duration": "4:19"
       }, {
-        "title": "Lucky.mp3",
-        "path": "songs/Lucky.mp3",
+        "title": "Lucky",
+        "path": "../../songs/Lucky.mp3",
         "duration": "3:45"
       }, {
-        "title": "Mad World.mp3",
-        "path": "songs/Mad World.mp3",
+        "title": "Mad World",
+        "path": "../../songs/Mad World.mp3",
         "duration": "2:39"
       }, {
-        "title": "Miss Ohio.mp3",
-        "path": "songs/Miss Ohio.mp3",
+        "title": "Miss Ohio",
+        "path": "../../songs/Miss Ohio.mp3",
         "duration": "2:29"
       }, {
-        "title": "Need You Now.mp3",
-        "path": "songs/Need You Now.mp3",
+        "title": "Need You Now",
+        "path": "../../songs/Need You Now.mp3",
         "duration": "3:21"
       }, {
-        "title": "Not Pretty Enough.mp3",
-        "path": "songs/Not Pretty Enough.mp3",
+        "title": "Not Pretty Enough",
+        "path": "../../songs/Not Pretty Enough.mp3",
         "duration": "2:58"
       }, {
-        "title": "Only Dreaming.mp3",
-        "path": "songs/Only Dreaming.mp3",
+        "title": "Only Dreaming",
+        "path": "../../songs/Only Dreaming.mp3",
         "duration": "3:05"
       }, {
-        "title": "Out Of My Head.mp3",
-        "path": "songs/Out Of My Head.mp3",
+        "title": "Out Of My Head",
+        "path": "../../songs/Out Of My Head.mp3",
         "duration": "2:28"
       }, {
-        "title": "Put Your Records On.mp3",
-        "path": "songs/Put Your Records On.mp3",
+        "title": "Put Your Records On",
+        "path": "../../songs/Put Your Records On.mp3",
         "duration": "3:34"
       }, {
-        "title": "Space Oddity.mp3",
-        "path": "songs/Space Oddity.mp3",
+        "title": "Space Oddity",
+        "path": "../../songs/Space Oddity.mp3",
         "duration": "3:59"
       }, {
-        "title": "Stars.mp3",
-        "path": "songs/Stars.mp3",
+        "title": "Stars",
+        "path": "../../songs/Stars.mp3",
         "duration": "3:18"
       }, {
-        "title": "Stay.mp3",
-        "path": "songs/Stay.mp3",
+        "title": "Stay",
+        "path": "../../songs/Stay.mp3",
         "duration": "2:56"
       }, {
-        "title": "Thank You.mp3",
-        "path": "songs/Thank You.mp3",
+        "title": "Thank You",
+        "path": "../../songs/Thank You.mp3",
         "duration": "2:52"
       }, {
-        "title": "The Story.mp3",
-        "path": "songs/The Story.mp3",
+        "title": "The Story",
+        "path": "../../songs/The Story.mp3",
         "duration": "3:51"
       }, {
-        "title": "Times Like These.mp3",
-        "path": "songs/Times Like These.mp3",
+        "title": "Times Like These",
+        "path": "../../songs/Times Like These.mp3",
         "duration": "3:27"
       }, {
-        "title": "What's Up.mp3",
-        "path": "songs/What's Up.mp3",
+        "title": "What's Up",
+        "path": "../../songs/What's Up.mp3",
         "duration": "3:20"
       }, {
-        "title": "When You Say Nothing At All.mp3",
-        "path": "songs/When You Say Nothing At All.mp3",
+        "title": "When You Say Nothing At All",
+        "path": "../../songs/When You Say Nothing At All.mp3",
         "duration": "2:41"
       }, {
-        "title": "White Flag.mp3",
-        "path": "songs/White Flag.mp3",
+        "title": "White Flag",
+        "path": "../../songs/White Flag.mp3",
         "duration": "3:18"
       }, {
-        "title": "Wonderwall.mp3",
-        "path": "songs/Wonderwall.mp3",
+        "title": "Wonderwall",
+        "path": "../../songs/Wonderwall.mp3",
         "duration": "4:43"
       }, {
-        "title": "Your Hands.mp3",
-        "path": "songs/Your Hands.mp3",
+        "title": "Your Hands",
+        "path": "../../songs/Your Hands.mp3",
         "duration": "3:02"
       }
-      // {title : "Song1.mp3"},
-      // {title : "Song2.mp3"},
-      // {title : "Song3.mp3"},
-      // {title : "Song4.mp3"},
-      // {title : "Song5.mp3"},
-      // {title : "Song6.mp3"},
-      // {title : "Song7.mp3"},
-      // {title : "Song8.mp3"},
-      // {title : "Song9.mp3"},
-      // {title : "Song10.mp3"},
-      // {title : "Song11.mp3"},
-      // {title : "Song12.mp3"},
-      // {title : "Song13.mp3"},
-      // {title : "Song14.mp3"},
-      // {title : "Song15.mp3"},
     ];
     return songs;
 
